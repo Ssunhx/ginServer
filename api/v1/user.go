2fba1392aeedbc0e8261597cae3bdaa9b83f49d6
孙红祥 <v-sunhongxiang@ainnovation.com>
2020-09-25 14:37:51 +0800

    <feat>(user): 用户登录接口

--- api/v1/user.go
+++ api/v1/user.go
@@ -1,6 +1,7 @@
 package v1
 
 import (
+	"ginserver/middleware"
 	"ginserver/model"
 	"ginserver/utils"
 	"github.com/gin-gonic/gin"
@@ -23,4 +24,28 @@ func AddUser(c *gin.Context) {
 		"message": utils.GetErrMsg(code),
 	})
 
+}
+
+func Login(c *gin.Context) {
+	// 登录接口，
+	var data model.User
+	_ = c.ShouldBindJSON(&data)
+
+	var token string
+	var code int
+
+	// 检查用户名和密码
+	code = model.CheckLogin(data.UserName, data.Password)
+	if code == utils.SUCCESS {
+		// 用户校验通过之后，生成 token，若验证未通过， token 为空
+		token, code = middleware.GenJwtToken(data.UserName)
+	} else {
+		code = utils.ERROR
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"status":  code,
+		"message": utils.GetErrMsg(code),
+		"data":    token,
+	})
 }
